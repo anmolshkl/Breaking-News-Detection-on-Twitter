0aa922753bb40230ec83999b4362035875c2511a
Anmol Shukla <anmol.shkl@gmail.com>
2016-04-08 21:49:05 +0530

    refactor old code to fit rabbitmq queues add callback to process tweets as they are fetched

--- LSH/main.py
+++ LSH/main.py
@@ -5,33 +5,40 @@ from buckets import Bucket, BucketsDB
 from datetime import datetime
 from operator import itemgetter
 import sys, json, utils, math
-import stream
 import get_stories
-import threading, collections, Queue
-import global_vars
+import pika
+
 
 print 'start 1'
-# start fetching the tweets and queue them up
-stream.run()
 
-print 'start 2'
 L             = 36
 K             = 13
 QUEUE_SIZE    = 20
 RECENT_TWEETS = 2000
 MIN_TOKENS    = 2
 
+# tweets consisting of these words would be ignored
 IGNORE = ['i', 'im', 'me', 'mine', 'you', 'yours']
 
+# tweets consisting these words would be picked --deprecated
 TAGS   = ['#news','#breakingnews']
-
-oovMap = {}
 
 buckets = BucketsDB(L, K, QUEUE_SIZE)
 recent  = RecentTweets(RECENT_TWEETS)
 
-unprocessed_tweet_queue = global_vars.tweet_queue 
-processed_tweet_queue   = global_vars.processed_tweet_queue
+# declare incoming channels, queues, exchanes for rabbitmq
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel    = connection.channel()
+channel.exchange_declare(exchange='tweets', type='direct')
+result     = channel.queue_declare(queue='FYP.Q.Cluster.NewsTweetMessage', durable=True)
+queue_name = result.method.queue
+channel.queue_bind(exchange='tweets', queue=queue_name, routing_key='news')
+
+# declare outgoing queue
+channel.queue_declare(queue='FYP.Q.Grouper.ClusteredTweetMessage', durable=True)
+
+print(' [*] Waiting for Tweets. To exit press CTRL+C')
+
 
 def getClosestNeighborBuckets(tweet):
     poss = buckets.getPossibleNeighbors(tweet)
@@ -61,75 +68,67 @@ def decideClosest(bcks, rec):
     else:
         return rec
 
-tweet     = None
+tweet_obj = None
 done      = 0
 starttime = 0
 systime   = datetime.now()
-
-with open(sys.argv[1]) as f:
-    for l in f:
-        ws        = l.split()
-        k         = unicode(ws[0])
-        v         = unicode(ws[1])
-        oovMap[k] = v
-
 skipped   = 0
-
 processed = 0
 
 
-print 'start 3'
+def callback(ch, method, properties, body):
+    global tweet_obj, done, starttime, skipped, processed, systime
 
-# start collecting stories in a seperate Thread
-t = threading.Thread(target=get_stories.run, args=())
-t.start()
-
-print 'start 4'
-
-while True:
-    if len(unprocessed_tweet_queue) != 0:
-        t     = unprocessed_tweet_queue.popleft()
-        # t   = json.loads(l)
-        msg   = t['tweet']
-        ts    = int(t['timestamp'])
-        msgid = int(t['id'])
-        uid   = int(t['user']['id'])
-        if utils.qualified(msg, TAGS, IGNORE, MIN_TOKENS):
-            tweet = Tweet(msg, ts, msgid, uid)
-            incr = TfIdf.getVals(tweet, oovMap)
-            # print tweet.getVector()
-            buckets.updateRndVec(incr)
-            closeBuck    = getClosestNeighborBuckets(tweet)
-            closeRecent  = getClosestNeighborRecent(tweet, closeBuck[1])
-            closeoverall = decideClosest(closeBuck,closeRecent)
-            other        = closeoverall[0]
-            if other:
-                t['nearneigh'] = other.msgid
-            else:
-                t['nearneigh'] = -1
-            t['cossim'] = closeoverall[1]
-            processed_tweet_queue.put(t) # add tweet to second queue
-            # sys.stdout.write(json.dumps(t) + '\n')
+    json_tweet     = json.loads(body)
+    sanitized_text = json_tweet['tweet']
+    
+    msg       = sanitized_text
+    ts        = int(json_tweet['timestamp'])
+    msgid     = int(json_tweet['id'])
+    uid       = int(json_tweet['user']['id'])
+    tweet_obj = Tweet(msg, ts, msgid, uid)
+    if utils.qualified(tweet_obj, TAGS, IGNORE, MIN_TOKENS):
+        incr = TfIdf.getVals(tweet_obj)
+        # print tweet.getVector()
+        buckets.updateRndVec(incr)
+        closeBuck    = getClosestNeighborBuckets(tweet_obj)
+        if closeBuck[0] is not None:
+            print "CLOSE BUCK: {0}, {1}".format(closeBuck[0].msg, closeBuck[1])
+        closeRecent  = getClosestNeighborRecent(tweet_obj, closeBuck[1])
+        closeoverall = decideClosest(closeBuck,closeRecent)
+        other        = closeoverall[0]
+        if other:
+            json_tweet['nearneigh'] = other.msgid
         else:
-            print 'skipped'
-            skipped += 1
+            json_tweet['nearneigh'] = -1
+        json_tweet['cossim'] = closeoverall[1]
+        channel.basic_publish(exchange='',
+                      routing_key='FYP.Q.Grouper.ClusteredTweetMessage',
+                      body=json.dumps(json_tweet),
+                      properties=pika.BasicProperties(
+                         delivery_mode = 2, # make message persistent
+                      ))
+        # sys.stdout.write(json.dumps(t) + '\n')
+    else:
+        print 'skipped'
+        skipped += 1
+    done += 1
+    current = int(ts) / 1000
+    if current-starttime > 900:
+        aftertime = datetime.now()
+        delta     = aftertime-systime
+        systime   = aftertime
+        dt        = divmod(delta.seconds,60)
+        sys.stderr.write(str(done) + ' Tweets done in ' + str(dt[0]) + ' min ' + str(dt[1]) + ' sec.\n')
+        starttime = current
 
+    processed += 1
+    if processed%100 == 0:
+        print '{0} tweets processed'.format(processed)
 
-        done += 1
-        current = int(ts) / 1000
-        if current-starttime > 900:
-            aftertime = datetime.now()
-            delta     = aftertime-systime
-            systime   = aftertime
-            dt        = divmod(delta.seconds,60)
-            sys.stderr.write(str(done) + ' Tweets done in ' + str(dt[0]) + ' min ' + str(dt[1]) + ' sec.\n')
-            starttime = current
-
-        processed += 1
-        if processed%100 == 0:
-            print '{0} tweets processed'.format(processed)
-    # else:
-    #     print 'Processor is waiting for tweets'
+if __name__ == '__main__':
+    channel.basic_consume(callback, queue=queue_name, no_ack=True)
+    channel.start_consuming()        
 
 sys.stderr.write('Processed ' + str(done) + ' tweets in total.\n')
 sys.stderr.write('Skipped a total of ' + str(skipped) + ' tweets.\n')
